90b100fb1a2ee11c0afffd5c7b4e0f5de7f4e7ee
mohali10895 <88863004+mohali10895@users.noreply.github.com>
2023-02-16 22:21:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,9 @@ var signupEmail = document.getElementById('signupEmail')
 var signupPassword = document.getElementById('signupPassword')
 var signinEmail = document.getElementById('signinEmail')
 var signinPassword = document.getElementById('signinPassword')
-var pathparts = location.pathname.split('/');
-var baseURL = ''
 
 
-for (var i = 0; i < pathparts.length - 1; i++) {
-    baseURL += '/' + pathparts[i]
-}
+
 
 // ============= signUp ================
 var signUpArray = []
@@ -85,14 +81,7 @@ function login() {
     for (var i = 0; i < signUpArray.length; i++) {
         if (signUpArray[i].email.toLowerCase() == email.toLowerCase() && signUpArray[i].password.toLowerCase() == password.toLowerCase()) {
             localStorage.setItem('itemUser', signUpArray[i].name)
-            location.replace('https://mohali10895.github.io/Smartlogin/home.html')
-//             if (baseURL == '/') {
-                
-
-//             } else {
-//                 location.replace(baseURL + '/home.html')
-
-//             }
+            location.replace('https://mohali10895.github.io/Smartlogin/home.html')            
         } else {
             document.getElementById('incorrect').innerHTML = '<span class="p-2 text-danger">incorrect email or password</span>'
         }
@@ -100,11 +89,11 @@ function login() {
 
 }
 
-// ============= logout ================
-function logout() {
-    localStorage.removeItem('itemUser')
-}
 var username = localStorage.getItem('itemUser')
 if (username) {
     document.getElementById('username').innerHTML = "Welcome " + username
+}
+// ============= logout ================
+function logout() {
+    localStorage.removeItem('itemUser')
 }
